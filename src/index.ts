3af715601f6f629e84bc9933336d8c64a67712bf
Adedunmola Oyewale <oyewaleadedunmola@gmail.com>
2024-01-05 19:10:12 +0100

    add commander

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
 #!/usr/bin/env node
+
+// import chalk from "chalk"
+// import ora  from "ora"
+
+// const spinner = ora("Asking chatgpt for suggestions...").start()
+
+// spinner.stop()
+
+// console.log(chalk.greenBright("Asked chatgpt..."))
+
+import { program } from "commander"
+
+
+program
+    .option("-c, --commit", "Commit changes")
+    .action(options => {
+        console.log(options)
+    })
+
+program.parse()
+
+const options = program.opts()
