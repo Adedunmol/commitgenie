30c62424b81be9c86b0c2c3fb0abd490f9d3900f
Adedunmola Oyewale <oyewaleadedunmola@gmail.com>
2024-01-06 17:37:51 +0100

    run git diff to get changes in files

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,38 @@
 #!/usr/bin/env node
 
-// import chalk from "chalk"
-// import ora  from "ora"
-
-// const spinner = ora("Asking chatgpt for suggestions...").start()
-
-// spinner.stop()
-
 // console.log(chalk.greenBright("Asked chatgpt..."))
 
+import { exec } from "child_process"
 import { program } from "commander"
+import ora  from "ora"
+import chalk from "chalk"
+import { resolve } from "path"
 
+const handleCommit = async (options: string) => {
+    const spinner = ora("Asking chatgpt for suggestions...").start()
+
+    // run a "git diff" command to get the diff across the repo
+
+    await new Promise(resolve => setTimeout(resolve, 5000))
+
+    exec("git diff", (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error}`)
+            return
+        }
+        if (stderr) {
+            console.log(`error: ${stderr}`)
+            return
+        }
+        console.log(stdout)
+    })
+
+    spinner.stop()
+}
 
 program
     .option("-c, --commit", "Commit changes")
-    .action(options => {
-        console.log(options)
-    })
+    .action(handleCommit)
 
 program.parse()
 
